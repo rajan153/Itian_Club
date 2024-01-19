ff85cf4349cdf35622922fae02d9164b3c163c7d
Rajan <rajanmasih154@gmail.com>
2024-01-19 18:05:29 +0530

    middlewares and routes added

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 
+const userRoutes = require("./routes/User.routes");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const dotenv = require("dotenv");
-const database = require("./configs/Database")
+const database = require("./configs/Database");
 
 dotenv.config();
 const PORT = process.env.PORT || 4000;
@@ -11,11 +14,19 @@ database.connect();
 
 // Default routes
 app.get("/", (req, res) => {
-    return res.status(200).json({
-      success: true,
-    });
+  return res.status(200).json({
+    success: true,
   });
-  
-  app.listen(PORT, () => {
-    console.log(`App is running on ${PORT}`);
-  });
+});
+
+app.listen(PORT, () => {
+  console.log(`App is running on ${PORT}`);
+});
+
+// middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: "*", credentials: true }));
+
+// Routes
+app.use("/api/v1/auth", userRoutes);
