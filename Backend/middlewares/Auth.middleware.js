e87c131fcc454e15ddc75e7ec7c8bae2480a7acd
Rajan <rajanmasih154@gmail.com>
2024-01-19 18:05:47 +0530

    auth middleware added

---
+++ Backend/middlewares/Auth.middleware.js
@@ -0,0 +1,74 @@
+const jwt = require("jsonwebtoken");
+
+// Auth
+exports.auth = async (req, res) => {
+  try {
+    const token =
+      req.cookies.token ||
+      req.body.token ||
+      req.header("Authorization").replace("Bearer ", "");
+
+    // If token is missing
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Token is missing.",
+      });
+    }
+
+    // verify token
+    try {
+      const decode = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = decode;
+    } catch (error) {
+      console.error(error);
+      res.status(401).json({
+        success: false,
+        message: "Token is invalid",
+      });
+    }
+    next();
+  } catch (error) {
+    console.error(error);
+    return res.status(401).json({
+      success: false,
+      message: "Something went wrong while validating the token",
+    });
+  }
+};
+
+// isStudent
+exports.isStudent = async (req, res, next) => {
+  try {
+    if (req.user.accountType !== "Student") {
+      return res.status(400).json({
+        success: false,
+        message: "This is protected route for Student",
+      });
+    }
+    next();
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "User role cannot be verified.",
+    });
+  }
+};
+
+// isAdmin
+exports.isAdmin = async (req, res, next) => {
+  try {
+    if (req.user.accountType !== "Admin") {
+      return res.status(400).json({
+        success: false,
+        message: "This is protected route for Admin",
+      });
+    }
+    next();
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "User role cannot be verified.",
+    });
+  }
+};
