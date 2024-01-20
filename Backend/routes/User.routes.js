2ef374ee4382f525a80b6f4ac8bfd1916a0c1dd2
Rajan <rajanmasih154@gmail.com>
2024-01-20 23:04:22 +0530

    login and changePassword routes added

--- Backend/routes/User.routes.js
+++ Backend/routes/User.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { sendOtp, signUp } = require("../controllers/User.controller");
+const { sendOtp, signUp, login, changePassword } = require("../controllers/User.controller");
 
 const { auth } = require("../middlewares/Auth.middleware");
 
@@ -10,10 +10,12 @@ const { auth } = require("../middlewares/Auth.middleware");
 // **************************************************************
 
 // User Login
-// router.post("/login", login);
+router.post("/login", login);
 // User SignUp
 router.post("/signup", signUp);
 // Sending Otp to User's email
 router.post("/sendotp", sendOtp);
+//  Change password
+router.post("/changepassword", auth, changePassword);
 
 module.exports = router;
